ce68b2bc0160dda70b03a86370dd29cdeba2d3f4
NaffMate <57339318+NaffMate@users.noreply.github.com>
2019-11-11 16:03:28 +1100

    Movement and Basic Level
    - Added Basic Horizontal and Vertical Movement on WASD input
    - Added Sprint Mechanic on Left Shift Key Hold
    - Added Jump and Double Jump on Space Bar
    - Physics Material added to increase friction between objects to decrease sliding of player
    - Level consisting of Plane and Basic jumping obstacle
    
    Double Jumping works but is not 100% perfect as the grounded bool is not set to false until the second jump input.

---
+++ The Growth of Samuel/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //public variables
+    public float speed;
+    public float sprintSpeed;
+    public bool grounded;
+
+    //private variables
+    private Rigidbody rb;
+    private Vector3 jump;
+    private float realSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0f, 10f, 0f);
+        grounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        realSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : speed;
+    }
+
+    private void FixedUpdate()
+    {
+        // sets the moveHorizontal and moveVertical variables as the horizontal and vertical inputs respectively
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // sets the movement in Vector3 for the ball movement
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // adds movement to the Rigidbody so the ball can move
+        rb.AddForce(movement * realSpeed);
+        movementJump();
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        //sets the grounded bool to true
+        grounded = true;
+    }
+
+    void movementJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            grounded = false;
+            rb.AddForce(jump, ForceMode.Impulse);
+            print("I'm jumping");
+        }
+    }
+}
